d11606e027309950fbeeba22ffb3e2f670b0218a
Junsang <kimnora070623@gmail.com>
2024-01-10 11:02:15 +0900

    UnityUI
    20240110

--- project_2024_01/Assets/Scripts/G_SingleTon.cs
+++ project_2024_01/Assets/Scripts/G_SingleTon.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class G_SingleTon : GenericSingleTon<G_SingleTon>
 {
+    //public Text text;
+    public TMP_Text tmp_text;                    // TMP Text 선언
     public int playerScore = 0;
+    public int playerScoreMax = 100;
+    public Slider mainSlider;           
     public void AddScore(int amount){
         playerScore += amount;
+        tmp_text.text = playerScore.ToString();  // playerScore는 int이기 때문에 문자열로 변환
+        mainSlider.value = (float)playerScore / (float)playerScoreMax;  // Slider의 값을 반영할 때
+    }
+
+    public void SubmitSliderValue(){                  // Slider에서 값을 가져올 때
+        tmp_text.text = mainSlider.value.ToString();
+    }
+
+    public void Start(){
+        //text.text = "Legacy";
+        tmp_text.text = "TMP";
     }
 }
